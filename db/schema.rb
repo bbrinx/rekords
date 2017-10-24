6b75a1994cc33e53c8689205279b3a15ba742d45
Jonathan <jonathan@decidata.tv>
2017-10-24 19:23:37 +0200

    Add category to records

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171024171618) do
+ActiveRecord::Schema.define(version: 20171024172104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,8 @@ ActiveRecord::Schema.define(version: 20171024171618) do
     t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_records_on_category_id"
   end
 
 end
