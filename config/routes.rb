7884d2374a9b454e5c12e3e5bda351e3e39b2181
rubyone <ruben54123@hotmail.com>
2017-11-06 18:13:37 +0100

    set up home view and first view styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
+
   root 'welcome#index'
 
   devise_for :profiles
+
+# root :to => 'homes#index'
+  resources :headers
+  resources :footers
+  resources :homes
+
   resources :records
   resources :categories
   resources :groups
   resources :profiles
+  resources :application
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
