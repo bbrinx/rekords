47b5a7bf97b096cdfe2df587634b0d60d720df14
rubyone <ruben54123@hotmail.com>
2017-11-06 18:13:37 +0100

    set up home view and first view styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def index
+    puts "hallo"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+root :to => 'homes#index'
+  resources :headers
+  resources :footers
+  resources :homes
   resources :records
   resources :categories
   resources :groups
   resources :profiles
+  resources :application
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
