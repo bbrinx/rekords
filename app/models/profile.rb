6a636c12b5f90e82de11402697fee8648975fbe6
Jonathan <jonathan@decidata.tv>
2017-11-11 16:42:17 +0100

    Fix profile creation by making profile -> grouops optional

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,6 +3,6 @@ class Profile < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  belongs_to :group
+  belongs_to :group, optional: true
   has_and_belongs_to_many :records
 end
