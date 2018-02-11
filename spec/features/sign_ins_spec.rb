58449cb25b82aedec96028a8f0966dde885d6195
Ch-W3st <ch.westphal@gmx.de>
2018-02-11 17:34:38 +0100

    fix sign-in test

--- spec/features/sign_ins_spec.rb
+++ spec/features/sign_ins_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
    context '(when logged in)' do
      before :each do
-       profile = Profile.create(email: 's0test@htw-berlin.de', password: '123456789123456789', password_confirmation: '123456789123456789')
+       profile = Profile.create(email: 'test@test.de', password: 'testtest', password_confirmation: 'testtest')
        login_as(profile, :scope => :profile)
       end
       it 'show home screen' do
         visit homes_path
-        expect(page).to have_content('Willkommen bei REKORDS')
+        expect(page).to have_content('Deine letzten Rekorde')
    end
 end
