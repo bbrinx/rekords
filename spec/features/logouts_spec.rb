b5de3ad9d728a6874dc03aae3c19c812859b8e69
Ch-W3st <ch.westphal@gmx.de>
2018-02-12 01:17:27 +0100

    add test logout

---
+++ spec/features/logouts_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+   context '(when logged in)' do
+     before :each do
+       profile = Profile.create(email: 's0test@htw-berlin.de', password: '123456789123456789', password_confirmation: '123456789123456789')
+       login_as(profile, :scope => :profile)
+      end
+      it 'check if logout works' do
+        visit destroy_profile_session_path
+        expect(page).to have_content('Log in')
+   end
+end
